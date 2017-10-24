f8dd9fd2559965aab377a3bd3a5a186f840a0b47
acampos91 <acampos91@hotmail.com>
2017-10-24 12:31:36 +0200

    jquery

---
+++ main.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+$('#menu > ul li:first').addClass('selected');
+$(".container article:nth-child(2)").addClass('middle');
+$(".list-item:odd").addClass('highlighted');
+});
